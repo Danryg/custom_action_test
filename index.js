819cf2d7ca83bae6e2b2e51152ff546663e3c1fb
root <root@DESKTOP-GOT3DS4>
2024-05-03 19:31:24 +0200

    Finished test action

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('node:fs');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+
+async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const body = core.getInput('body');
+    fs.writeFileSync('body.txt', body);
+    await exec.exec('git add body.txt');
+    const cmd = 'git commit -m "' + body + '"';
+    await exec.exec(cmd);
+    await exec.exec('git push');
+    core.setOutput("out", body);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+
+run();
